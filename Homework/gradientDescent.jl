70244972a297d6b7cebdfa294a63c595f81c550c
rmtrane <rmtrane@gmail.com>
2018-11-28 15:16:00 -0600

    small updates

--- Homework/gradientDescent.jl
+++ Homework/gradientDescent.jl
@@ -15,22 +15,61 @@ function SingleStepGS(alpha,A,x,b)
 end
 
 ## Gradient descent for Strategy 1: alpha = ||A'r_c||^2/||AA'r_c||^2
-function gradientDescentStrat1(A,b,x,xstar;epsilon=1e-15, maxIter=1000)
+function gradientDescentStrat1(A,b,x,x0;epsilon=1e-14, maxIter=10000)
     # Get current residual
-    rc = A*x-b
+    rc = A*x0-b
     # Get current error
-    ec = norm(xstar - x)
-    i = 0
-    while norm(A'rc) >= epsilon && i < maxIter
-        i += 1
-        print(i)
+    ec = norm(x0 - x)
+    i = 1
+
+    while norm(rc) >= epsilon && i < maxIter
         alpha = norm(A'*rc)^2/norm(A*A'*rc)^2
-        x = SingleStepGS(alpha,A,x,b)
-        rc = A*x-b
-        ec = vcat(ec, norm(x - xstar))
+        x0 = SingleStepGS(alpha,A,x0,b)
+        rc = A*x0-b
+        ec = vcat(ec, norm(x0 - x))
+        i += 1
     end
 
-    return x, ec
+    return x0, ec
+end
+
+## Gradient descent for Strategy 2: alpha = ||A'(x_c - x^*)||^2/||AA'(x_c-x^*)||^2
+function gradientDescentStrat2(A,b,x,x0;epsilon=1e-14, maxIter=10000)
+    # Get current residual
+    rc = A*x0-b
+    # Get current error
+    ec = norm(x0 - x)
+    i = 1
+
+    while norm(rc) > epsilon && i < maxIter
+        alpha = norm(A'*(x0-x))^2/norm(A*A'*(x0-x))^2
+        x0 = SingleStepGS(alpha,A,x0,b)
+        rc = A*x0-b
+        ec = vcat(ec, norm(x0 - x))
+        i += 1
+    end
+
+    return x0, ec
+end
+
+## Gradient descent for Strategy 3: alpha = ||A||^2/||AA'||^2
+function gradientDescentStrat3(A,b,x,x0;epsilon=1e-14, maxIter=10000)
+    # Get current residual
+    rc = A*x0-b
+    # Get current error
+    ec = norm(x0 - x)
+    i = 1
+    alpha = norm(A)^2/norm(A*A')^2
+
+    while norm(rc) >= epsilon && i < maxIter
+        #print(norm(A'*rc))
+        x0 = SingleStepGS(alpha,A,x0,b)
+        rc = A*x0-b
+        ec = vcat(ec, norm(x0 - x))
+        i += 1
+    end
+
+    return x0, ec
 end
 
 ## Function to generate problem
@@ -43,21 +82,39 @@ function generateProblem(n)
     return A, b, x, x0
 end
 
-A,b,x,x0 = generateProblem(10)
-gradientDescentStrat1(A,b,x,x0)
+using Distributions; using Random
+Random.seed!(0)
+problems = map(x-> generateProblem(sample(5:10)), 1:20)
 
-function gradientDescent1(A,x,b, xreal;alpha_strategy = 1,ϵ = 1e-15,maxIter=1000)
-    k = 1
-    r_c = A*x - b
-    r = norm(x - xreal)
-    while norm(A'* r_c) >= ϵ && k <= maxIter
-        alpha = norm(A'*r_c)^2 / norm(A*A'*r_c)^2
-        x = singleGradDescent(A,x,b,alpha)
-        r_c = A*x - b
-        k = k+1
-        r = vcat(r, norm(x - xreal))
-    end
-    return x, r, k
+A,b,x,x0 = problems[1]
+
+gradientDescentStrat1(A,b,x,x0)
+gradientDescentStrat2(A,b,x,x0)
+gradientDescentStrat3(A,b,x,x0)
+
+
+
+function generateProblemEigenvalues(eigenvalues)
+    n = length(eigenvalues)
+    B = rand(n,n)
+    Q = qr(B).Q
+    Lambda = diagm(0 => eigenvalues)
+    A = Q*Lambda*Q'
+    x = randn(n)
+    b = A*x
+    return A, x, b
 end
 
-gradientDescent1(A,x,b,x0)
+eigenvalues = rand(4)
+
+A = generateProblemEigenvalues(eigenvalues)[1]
+
+sqrt.(svdvals(A'*A))
+
+Vt = svd(A'*A).Vt
+
+U = svd(A'*A).U
+
+
+
+eigen(A).vectors
